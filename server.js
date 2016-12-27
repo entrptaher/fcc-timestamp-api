b5b0b7642a8eee7334202886f5b18c92f9d95f34
Abu Taher <entrptaher@gmail.com>
2016-12-27 14:27:08 +0000

    Basic Backend Done

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 var express = require('express')
 var app = express()
+app.set('view engine', 'pug')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function(req,res){
+    res.render('index', { title: 'Timestamp API', message: 'Hello there!' })
+})
+
+app.get('/:time', function (req, res) {
+    // empty array
+    var data={'unix':'', 'natural':""};
+    var inputtime = req.params.time;
+    
+    //check if this is a UTC time
+    if(!isNaN(Date.parse(inputtime))){
+            data.unix = Date.parse(inputtime)/1000
+            data.natural = new Date(inputtime).toUTCString();
+    }
+    // Check if this is a unix timestamp
+    else if(!isNaN(new Date(inputtime*1000))){
+        data.unix = inputtime
+        data.natural = new Date(inputtime*1000).toUTCString();
+    }
+    else{
+        data.unix = 'null'
+        data.natural = 'null'
+    }
+    res.send(data)
 })
 
 app.listen(8080, function () {
